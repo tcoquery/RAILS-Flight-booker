07f2b4fdcb4c45bea76946358e3502b96e6f3f89
tcoquery <tcoquery@gmail.com>
2022-08-01 15:27:52 +0200

    remove message when airports are the same

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,14 +6,11 @@ class FlightsController < ApplicationController
     @date_options = Flight.all.map { |f| f.takeoff_date }.uniq
     @time_options = Flight.all.map { |f| f.takeoff_time }.uniq
 
-    @search_results = nil
-
+  
     if params[:departure_airport] != params[:arrival_airport] 
       @departure_airport = Airport.where("airport_code = ?", params[:departure_airport]).first.id
       @arrival_airport = Airport.where("airport_code = ?", params[:arrival_airport]).first.id
       @search_results = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND takeoff_date = ?", @departure_airport, @arrival_airport, params[:date])
-    elsif params[:departure_airport] == params[:arrival_airport] 
-      @search_results = []
     end
   end
 end
